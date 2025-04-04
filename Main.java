668a60a220bc4d18748fbe771d48d013c8ed3d87
Diogo Frota <diogofrota@icloud.com>
2025-04-04 15:27:01 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ControleRemoto c1 = new ControleRemoto();
+        System.out.println("---- Menu ----");
+        c1.ligar();
+        c1.abrirMenu();
+    }
+}
